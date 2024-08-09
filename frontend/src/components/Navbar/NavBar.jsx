24fe6691eeaf8e0822095672635d7706616db2c5
mohit0317 <mohitkhodse44@gmail.com>
2024-08-09 23:58:00 +0530

    navbar

--- frontend/src/components/Navbar/NavBar.jsx
+++ frontend/src/components/Navbar/NavBar.jsx
@@ -1,12 +1,24 @@
 import React from 'react';
-import './NavBar'
+import './NavBar.css';
+import { assets } from './../../assets/assets';
+import { Container, Button } from 'react-bootstrap';
 
 const NavBar = () => {
     return (
-        <div className='navbar' >
+        <Container className='navbar'>
+            <img src={assets.logo} alt="Logo" className='logo' />
+            <ul className='navbar-menu'>
+                <li className='navbar-item'>Menu</li>
+                <li className='navbar-item'>Mobile</li>
+                <li className='navbar-item'>Home</li>
+                <li className='navbar-item'>Contact Us</li>
+            </ul>
+            <div className='navbar-right'>
+                <img src={assets.search_icon} alt="Search" className='search-icon' />
+                <img src={assets.basket_icon} alt="Basket" className='basket-icon' />
+                <Button className='signin-button' variant="outline-warning"><span style={{ color: 'black' }}>sign in</span></Button>{' '}            </div>
+        </Container>
+    );
+};
 
-        </div>
-    )
-}
-
-export default NavBar
+export default NavBar;
